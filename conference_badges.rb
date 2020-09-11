3f9766814c93259eb7f906591912947502a4af1e
Kemunto Maragia <kmaragia@email.wm.edu>
2020-09-11 02:12:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,19 +1,28 @@
 def badge_maker(name)
-  puts "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
-  
+def batch_badge_creator(attendees)
+  new_arr = []
+  attendees.each do |x|
+    new_arr << "Hello, my name is #{x}."
+  end
+  return new_arr
 end
 
-def assign_rooms(array)
-  
-end
-  
-def printer()
-  return 
-  
+def assign_rooms(speakers)
+  new_arr = []
+  speakers.each do |x|
+    new_arr << "Hello, #{x}! You'll be assigned to room #{speakers.index(x) + 1}!"
+  end
+  return new_arr
 end
 
-  
-  
+def printer(attendees)
+  batch_badge_creator(attendees).each do |x|
+    puts x 
+  end
+  assign_rooms(attendees).each do |x|
+    puts x 
+  end
+end
